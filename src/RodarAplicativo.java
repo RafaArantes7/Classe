5977f55a78e1446b9f5c9487bd8036ff3f29ece8
Perkles <rafael.arantes7@hotmail.com>
2022-11-29 11:43:58 -0300

    Orientação Objetos

--- src/RodarAplicativo.java
+++ src/RodarAplicativo.java
@@ -1,19 +1,19 @@
+package certificadoDio
+
 class RodarAplicativo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        carro carro1 = new carro();
+        Carro carro1 = new Carro();
 
-        carro1.setCor(Azul);
-        carro1.setModelo(BMW 3);
-        carro1.setCapacidadeTanque(59);
+        carro1.setCor("Azul");
+        carro1.setModelo("BMW 3");
+        carro1.setCapacidadeTanque("59");
 
         System.out.println(carro1.getCor());
         System.out.println(carro1.getModelo());
         System.out.println(carro1.getCapacidadeTanque());
         System.out.println(carro1.totalValorTanque(5.45));
-
-       
 
     }
 
