c84530fc6974df3b7088d22a9829489c736eb66f
Perkles <rafael.arantes7@hotmail.com>
2022-11-29 11:29:15 -0300

    Orientação Objeto

---
+++ src/RodarAplicativo.java
@@ -0,0 +1,20 @@
+class RodarAplicativo {
+
+    public static void main(String[] args){
+
+        carro carro1 = new carro();
+
+        carro1.setCor(Azul);
+        carro1.setModelo(BMW 3);
+        carro1.setCapacidadeTanque(59);
+
+        System.out.println(carro1.getCor());
+        System.out.println(carro1.getModelo());
+        System.out.println(carro1.getCapacidadeTanque());
+        System.out.println(carro1.totalValorTanque(5.45));
+
+       
+
+    }
+
+}
